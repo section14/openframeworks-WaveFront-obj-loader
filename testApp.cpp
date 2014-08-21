b1c41859ce5ce16ce7e79dc8eeca943abf995c2b
section14 <henduism@gmail.com>
2014-08-21 19:06:03 -0400

    Create testApp.cpp

---
+++ testApp.cpp
@@ -0,0 +1,34 @@
+#include "testApp.h"
+
+//--------------------------------------------------------------
+void testApp::setup()
+{
+    glEnable(GL_DEPTH_TEST);
+    glEnable(GL_LIGHTING);
+
+    ofBackground(10, 10, 10);
+
+    camera.setDistance(10);
+    light.setPosition(10,30,-25);
+
+    objectLoader = new waveFrontLoader();
+    objectLoader->loadFile("test.obj");
+    mesh = objectLoader->generateMesh();
+    delete objectLoader;
+}
+
+//--------------------------------------------------------------
+void testApp::update()
+{
+
+}
+
+//--------------------------------------------------------------
+void testApp::draw()
+{
+    camera.begin();
+    light.enable();
+    mesh.draw();
+    light.disable();
+    camera.end();
+}
