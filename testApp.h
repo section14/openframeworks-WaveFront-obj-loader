e7144cab9b8ee4f006d1a2f34cde8abb36955019
section14 <henduism@gmail.com>
2014-08-21 19:04:50 -0400

    Create testApp.h

---
+++ testApp.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "ofMain.h"
+#include "waveFrontLoader.h"
+
+class testApp : public ofBaseApp{
+
+	public:
+		void setup();
+		void update();
+		void draw();
+
+		waveFrontLoader *objectLoader;
+        ofMesh mesh;
+        ofEasyCam camera;
+        ofLight light;
+
+};
