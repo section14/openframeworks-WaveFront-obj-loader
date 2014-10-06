9d08962f6fb6580f5315bf9a69cb485647838b92
section14 <henduism@gmail.com>
2014-10-06 13:45:22 -0400

    Update waveFrontLoader.cpp
    added ability to parse and use .mtl files

--- waveFrontLoader.cpp
+++ waveFrontLoader.cpp
@@ -3,22 +3,70 @@
 waveFrontLoader::waveFrontLoader()
 {
     indexCount = 0;
+    faceCount = 0;
+    materialColorCount = 0;
+    materialCount = 0;
+    materialBool = false;
 }
 
 void waveFrontLoader::loadFile(const char *fileName)
 {
-    ifstream file;
+    //if we're using a material file, load it
+    if (materialBool == true)
+    {
+        ifstream matFile;
+        char matLine[255];
+
+        char materialFile[255];
+        strncpy(materialFile, fileName, sizeof(materialFile));
+        strncat(materialFile, ".mtl", sizeof(materialFile) - strlen(materialFile) - 1);
+
+        //open file in openframeworks data folder
+        matFile.open(ofToDataPath(materialFile).c_str());
+
+        if (matFile.is_open())
+        {
+            while (matFile.getline(matLine, 255))
+            {
+                parseMaterial(matLine);
+            }
+        }
+
+        matFile.close();
+
+    }
+
+    //get filename and open corresponding .obj file
+    ifstream objFile;
     char line[255];
 
-    //open file in openframeworks data folder
-    file.open(ofToDataPath(fileName).c_str());
+    char objectFile[255];
+    strncpy(objectFile, fileName, sizeof(objectFile));
+    strncat(objectFile, ".obj", sizeof(objectFile) - strlen(objectFile) - 1);
 
-    if (file.is_open())
+    //open file in openframeworks data folder
+    objFile.open(ofToDataPath(objectFile).c_str());
+
+    if (objFile.is_open())
     {
-        while (file.getline(line,255))
+        while (objFile.getline(line,255))
         {
             parseLine(line);
         }
+    }
+
+    objFile.close();
+}
+
+void waveFrontLoader::loadMaterial(bool load)
+{
+    if (load == true)
+    {
+        materialBool = true;
+    }
+    else
+    {
+        materialBool = false;
     }
 }
 
@@ -34,7 +82,11 @@ void waveFrontLoader::parseLine(char *line)
     char *lineType = strtok(strdup(line), " ");
 
     //parse line depending on type
-    if (!strcmp(lineType, "v")) // Vertices
+    if (!strcmp(lineType, "usemtl")) // Use Material
+    {
+        parseMaterialLocation(line);
+    }
+    else if (!strcmp(lineType, "v")) // Vertices
     {
         parseVertex(line);
     }
@@ -76,7 +128,9 @@ void waveFrontLoader::parseFace(char *line)
 {
     //var to hold and discard un-needed texture data
     int discard;
-    
+
+    faceCount++;
+
     indices.push_back(Index());
 
     //get vertex and normal indices, discard texture data for now
@@ -101,20 +155,85 @@ void waveFrontLoader::parseFace(char *line)
     }
 }
 
+void waveFrontLoader::parseMaterialLocation(char *line)
+{
+    materialCount++;
+
+    char materialName[255];
+
+    sscanf(line, "usemtl %s", materialName);
+
+    materialLocations[faceCount] = materialName;
+}
+
+void waveFrontLoader::parseMaterial(char *line)
+{
+    //If empty, don't do anything with it
+    if(!strlen(line))
+    {
+        return;
+    }
+
+    //get line type identifier from char string
+    char *lineType = strtok(strdup(line), " ");
+
+
+    if (!strcmp(lineType, "newmtl")) // Material Name
+    {
+        sscanf(line, "newmtl %s", tempMatName);
+        materialColorCount++;
+    }
+    else if (!strcmp(lineType, "Ka")) // Ambient Color
+    {
+        sscanf(line, "Ka %f %f %f", &tempColor.ambientColor.r, &tempColor.ambientColor.g, &tempColor.ambientColor.b);
+    }
+    else if (!strcmp(lineType, "Kd")) // Diffuse Color
+    {
+        sscanf(line, "Kd %f %f %f", &tempColor.diffuseColor.r, &tempColor.diffuseColor.g, &tempColor.diffuseColor.b);
+    }
+    else if (!strcmp(lineType, "Ks")) // Specular Color
+    {
+        sscanf(line, "Ks %f %f %f", &tempColor.specularColor.r, &tempColor.specularColor.g, &tempColor.specularColor.b);
+    }
+    else if (!strcmp(lineType, "d")) // Alpha
+    {
+        sscanf(line, "d %f", &tempColor.alpha);
+    }
+    else if (!strcmp(lineType, "illum")) // End of material signifier
+    {
+        //it's the end of this material, so add to material map, and reset vars
+        materialColors[tempMatName] = tempColor;
+        tempColor = emptyColor;
+    }
+}
+
 ofMesh waveFrontLoader::generateMesh()
 {
+    int materialCount = materialColors.size();
+    int localIndexCount = 0;
+
+    ofColor vertColor;
+    ofColor defaultColor = ofColor(0.0, 255.0, 0.0);
+
     for (std::vector<Index>::iterator i = indices.begin(); i != indices.end(); ++i)
     {
-        //check if the vertex and normal location exists already
-        addFace(&vertices[(i->v1) - 1], &normals[(i->vn1) - 1]);
-        addFace(&vertices[(i->v2) - 1], &normals[(i->vn2) - 1]);
-        addFace(&vertices[(i->v3) - 1], &normals[(i->vn3) - 1]);
+        if (materialLocations[localIndexCount] != "")
+        {
+            vertColor = mapColor(materialColors[materialLocations[localIndexCount]].diffuseColor);
+        }
+
+        //draw triangles
+        addFace(&vertices[(i->v1) - 1], &normals[(i->vn1) - 1], &vertColor);
+        addFace(&vertices[(i->v2) - 1], &normals[(i->vn2) - 1], &vertColor);
+        addFace(&vertices[(i->v3) - 1], &normals[(i->vn3) - 1], &vertColor);
+
+        localIndexCount++;
     }
 
     return mesh;
 }
 
-void waveFrontLoader::addFace(ofVec3f *vertex, ofVec3f *normal)
+void waveFrontLoader::addFace(ofVec3f *vertex, ofVec3f *normal, ofColor *color)
 {
     //temp vars
     int vertIndex;
@@ -156,6 +275,9 @@ void waveFrontLoader::addFace(ofVec3f *vertex, ofVec3f *normal)
         mesh.addVertex(*vertex);
         faceVertices[currentIndex] = *vertex;
 
+        //add color
+        mesh.addColor(*color);
+
         //add normal
         mesh.addNormal(*normal);
         faceNormals[currentIndex] = *normal;
@@ -163,6 +285,18 @@ void waveFrontLoader::addFace(ofVec3f *vertex, ofVec3f *normal)
         //add index
         mesh.addIndex(currentIndex);
     }
+
+}
+
+ofColor waveFrontLoader::mapColor(colorRGB vertexColor)
+{
+    float red = ofMap(vertexColor.r, 0.0, 1.0, 0.0, 255.0);
+    float green = ofMap(vertexColor.g, 0.0, 1.0, 0.0, 255.0);
+    float blue = ofMap(vertexColor.b, 0.0, 1.0, 0.0, 255.0);
+
+    ofColor newColor = ofColor(red, green, blue);
+
+    return newColor;
 }
 
 waveFrontLoader::~waveFrontLoader()
