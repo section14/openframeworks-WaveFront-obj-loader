d9a1f99908d990a3e1a2bdfe8729066e1916626f
section14 <henduism@gmail.com>
2014-10-06 13:44:45 -0400

    Update waveFrontLoader.h
    added ability to parse exported .mtl files

--- waveFrontLoader.h
+++ waveFrontLoader.h
@@ -13,6 +13,7 @@ public:
     ~waveFrontLoader();
 
     void loadFile(const char *fileName);
+    void loadMaterial(bool load);
     ofMesh generateMesh();
 
 private:
@@ -24,21 +25,53 @@ private:
     }
     Index;
 
+    typedef struct
+    {
+        float r;
+        float g;
+        float b;
+    }
+    colorRGB;
+
+    typedef struct
+    {
+        colorRGB ambientColor;
+        colorRGB diffuseColor;
+        colorRGB specularColor;
+        float alpha;
+    }
+    matColor;
+
     std::vector<ofVec3f> vertices;
     std::vector<ofVec3f> normals;
     std::vector<Index> indices;
 
     std::map<int,ofVec3f>faceVertices;
     std::map<int,ofVec3f>faceNormals;
+    std::map<string,matColor> materialColors;
+    std::map<int,string>materialLocations;
 
     int indexCount;
+    int faceCount;
+    int materialColorCount;
+    int materialCount;
+    bool materialBool;
+
+    char tempMatName[255];
+    matColor tempColor;
+    matColor emptyColor;
 
     void parseLine(char *line);
     void parseVertex(char *line);
     void parseNormal(char *line);
     void parseFace(char *line);
 
-    void addFace(ofVec3f *vertex, ofVec3f *normal);
+    void parseMaterialLocation(char *line);
+    void parseMaterial(char *line);
+
+    void addFace(ofVec3f *vertex, ofVec3f *normal, ofColor *color);
+
+    ofColor mapColor(colorRGB vertexColor);
 };
 
 #endif
